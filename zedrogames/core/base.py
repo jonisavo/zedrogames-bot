69425a5d0f9b441cad18c48ee185dd89b6c76efa
jonisavo <12786937+jonisavo@users.noreply.github.com>
2020-08-02 18:00:47 +0300

    Updated !help and !message to use Member#send instead of Member#create_dm

--- zedrogames/core/base.py
+++ zedrogames/core/base.py
@@ -15,10 +15,8 @@ class BaseCommands(commands.Cog):
         # Delete the received command message
         await self.bot.delete_message(ctx.message)
         # Send a nice message
-        await ctx.author.create_dm()
-        dm = ctx.author.dm_channel
         todd_file = discord.File("assets/todd.png", filename="TODD.png")
-        await dm.send("In the triple-A games industry, no one can hear you scream", file=todd_file)
+        await ctx.author.send("In the triple-A games industry, no one can hear you scream", file=todd_file)
 
     @commands.command(name='message')
     @commands.guild_only()
@@ -29,11 +27,9 @@ class BaseCommands(commands.Cog):
         # Delete the received command message
         await self.bot.delete_message(ctx.message)
         channels = await guildutils.get_text_channels(ctx.guild)
-        await ctx.author.create_dm()
-        dm = ctx.author.dm_channel
         # If there are no text channels to send messages into, return
         if len(channels) == 0:
-            dm.send(
+            ctx.author.send(
                 ("Hey there. You tried to send a message, but there aren't any "
                  f"text channels in {ctx.guild.name} to send it into.")
             )
@@ -43,20 +39,20 @@ class BaseCommands(commands.Cog):
                    "Select which channel you wish to send the message into.")
         for channel in channels:
             dm_text += f"\n{channels.index(channel)}: #{channel.name}"
-        await dm.send(dm_text)
+        await ctx.author.send(dm_text)
         # read_index_reply starts a query with a 30 second timer. If it expires, None is returned.
         selected_index = await msgutils.read_index_reply(ctx, channels)
         if selected_index is None:
             return
         selected_channel = channels[selected_index]
-        await dm.send(f"You chose #{selected_channel.name} as the output channel.")
-        await dm.send(f"Write any message, and it will be sent to #{selected_channel.name}. You have five minutes.")
+        await ctx.author.send(f"You chose #{selected_channel.name} as the output channel.")
+        await ctx.author.send(f"Write any message, and it will be sent to #{selected_channel.name}. You have 5 minutes.")
         # Write a message for the bot to send
         try:
             message = await self.bot.wait_for('message', timeout=300.0, check=lambda msg:
             msg.author == ctx.author and isinstance(msg.channel, discord.DMChannel))
         except asyncio.TimeoutError:
-            await dm.send("The query timed out.")
+            await ctx.author.send("The query timed out.")
             return
 
         # TODO get attachments to work
@@ -72,31 +68,31 @@ class BaseCommands(commands.Cog):
 
         # At the moment, disallow sending empty messages
         if len(message.content) == 0:
-            dm.send("You tried to send an empty message!")
+            ctx.author.send("You tried to send an empty message!")
             return
         # Ask for confirmation
-        await dm.send(content=message.content, tts=message.tts)
-        await dm.send(f"Send this message to #{selected_channel.name}? Y/N")
+        await ctx.author.send(content=message.content, tts=message.tts)
+        await ctx.author.send(f"Send this message to #{selected_channel.name}? Y/N")
         try:
             confirmation = await self.bot.wait_for('message', timeout=30.0, check=lambda msg:
                                     msg.author == ctx.author and isinstance(msg.channel, discord.DMChannel) and
                                     (msg.content.lower().startswith("y") or msg.content.lower().startswith("n")))
         except asyncio.TimeoutError:
-            await dm.send("The query timed out.")
+            await ctx.author.send("The query timed out.")
             return
         # If the user declined, return
         if confirmation.content.lower().startswith("n"):
-            await dm.send("The operation was cancelled.")
+            await ctx.author.send("The operation was cancelled.")
             return
         # Attempt to send the message to the chosen channel
         try:
             await selected_channel.send(content=message.content, tts=message.tts)
         except discord.Forbidden:
-            await dm.send("I could not send the message (error code 403: Forbidden). Do I have the necessary permissions?")
+            await ctx.author.send("I could not send the message (403: Forbidden). Do I have the necessary permissions?")
         except discord.HTTPException as e:
-            await dm.send(f"I could not send the message (error code {e.status}: {e.text})")
+            await ctx.author.send(f"I could not send the message (error code {e.status}: {e.text})")
         else:
-            await dm.send("The message was sent.")
+            await ctx.author.send("The message was sent.")
             print(f"A message was sent to {ctx.guild.name}>#{selected_channel.name} by {ctx.author}")
 
 
